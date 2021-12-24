94b13bae353e8c21efa8674a9ce3adaad5b99799
Jason Evans <evans.jason@hotmail.co.uk>
2021-12-24 21:36:55 +0000

    local storage configured, delete button added

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+let inputValues = [];
+const inputEl = document.getElementById('input--el')
+const inputBtn = document.getElementById("list--button")
+const deleteBtn = document.getElementById('delete--button')
 const ulEl = document.getElementById('ul--el')
 
-function input(){
-    let inputVal = document.getElementById('list--input').value
-    ulEl.textContent = inputVal;
+const inputValuesFromLocalStorage = JSON.parse( localStorage.getItem("inputValues") )
+
+if (inputValuesFromLocalStorage) {
+    inputValues = inputValuesFromLocalStorage
+    renderVal()
 }
+
+function renderVal() {
+    let listItems = ""
+    for (let i = 0; i < inputValues.length; i++) {
+        listItems += `<li> ${inputValues[i]} </li>`
+        console.log(inputValues[i])
+    }
+    
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    inputValues = []
+    renderVal()
+} )
+
+inputBtn.addEventListener("click", function(){
+    inputValues.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("inputValues", JSON.stringify(inputValues) )
+    renderVal()
+    console.log(inputValues)
+})
+
+
+
+
+
 
